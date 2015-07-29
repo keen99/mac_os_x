b64f52b71bb1ac62a407109fe8d8121d2bccd40a
david raistrick <david.raistrick@fanduel.com>
2015-07-29 19:17:01 -0400

    hack some arg handling in for magic kills

--- definitions/updatedefaults.rb
+++ definitions/updatedefaults.rb
@@ -1,10 +1,13 @@
 
 
 #this should probably take an arg:  what array to process... or just take the array.
+#and what to kill.
 
-define :updatedefaults do
+define :updatedefaults :array => node['mac_os_x']['settings'], :killwhat => [] do
+
 
   ## ignore failure - depending on login state, these might not be running when chef runs.
+
   execute "killall Dock" do
     ignore_failure true
     action :nothing
@@ -18,7 +21,17 @@ define :updatedefaults do
     action :nothing
   end
 
-  node['mac_os_x']['settings'].each do |domain,settings|
+
+  params[:killwhat].each do |killwhat|
+    execute "killall #{killwhat}" do
+      ignore_failure true
+      action :nothing
+    end
+  end
+
+
+
+  params[:array].each do |domain,settings|
     settings.each do |k,v|
       next if k == 'domain'
 
@@ -29,6 +42,8 @@ define :updatedefaults do
         value v
         sudo true if settings['domain'] =~ /^\/Library\/Preferences/
         global true if settings['domain'] =~ /^NSGlobalDomain$/
+##uh for multiples.. need a loop here
+       notifies :run, "execute[killall #{params[:killwhat].to_s}]" if params[:killwhat].is_set
         notifies :run, "execute[killall Dock]" if settings['domain'] =~ /^com.apple.dock$/
         notifies :run, "execute[killall Dock]" if settings['domain'] =~ /^com.apple.dashboard$/
         notifies :run, "execute[killall Finder]" if settings['domain'] =~ /^com.apple.finder$/
