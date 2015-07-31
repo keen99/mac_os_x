74f9e8627112692cd110b33200cf5e886c744f60
david raistrick <david.raistrick@fanduel.com>
2015-07-31 17:13:36 -0400

    try to take an array param

--- definitions/updatedefaults.rb
+++ definitions/updatedefaults.rb
@@ -19,6 +19,19 @@ define :updatedefaults, :processwhat => [], :killwhat => [] do
 
   processwhat=node['mac_os_x']['settings']
 
+  ## 2 for the default, 1 for something we pass in.
+  def hash_depth(hash)
+    a = hash.to_a
+    d = 1
+    while (a.flatten!(1).map! {|e| (e.is_a? Hash) ? e.to_a.flatten(1) : (e.is_a? Array) ? e : nil}.compact!.size > 0)
+        d += 1
+    end
+    return d
+  end
+
+  ##find the depth of the mash we were handed.
+  depth=hash_depth(processwhat)
+
 if params[:processwhat].to_a.empty?
   processwhat=node['mac_os_x']['settings']
   log "updatedefaults: using default"
@@ -30,97 +43,75 @@ else
   log "updatedefaults: using param"
 end
 
-##find the depth of our array we were handed.
 
-  arr=processwhat.to_a
-  b, depth = arr.dup, 1
-  until b==arr.flatten
-    depth+=1
-    b=b.flatten(1)
+
+
+
+
+log "updatedefaults: updating |depth #{depth}| [[ #{processwhat} ]]"
+
+
+
+  ## ignore failure - depending on login state, these might not be running when chef runs.
+
+  execute "killall Dock" do
+    ignore_failure true
+    action :nothing
   end
-puts "#{mode} Array depth: #{depth}" #=> 4
-
-## feh, depth = 2 in both cases.
-
-
-def count_subarrays array
-  return 0 unless array && array.is_a?(Array)
-
-  nested = array.select { |e| e.is_a?(Array) }
-  if nested.empty?
-    1 # this is a leaf
-  else
-    nested.inject(0) { |sum, ary| sum + count_subarrays(ary) }
+  execute "killall Finder" do
+    ignore_failure true
+    action :nothing
   end
-end
+  execute "killall loginwindow" do
+    ignore_failure true
+    action :nothing
+  end
 
-def hash_traverse(hash)
-        a = hash.to_a
-        d = 1
-        while (a.flatten!(1).map! {|e| (e.is_a? Hash) ? e.to_a.flatten(1) : (e.is_a? Array) ? e : nil}.compact!.size > 0)
-            d += 1
+
+  params[:killwhat].each do |killwhat|
+    log "DSR: killwhat #{killwhat}"
+    execute "killall #{killwhat}" do
+      ignore_failure true
+      action :nothing
+    end
+  end
+
+
+  #pull this out of the outer loop so we can call it instead.
+    def settingsloop(settings)
+      settings.each do |k,v|
+        next if k == 'domain'
+
+        mac_os_x_userdefaults "#{settings['domain']}-#{k}" do
+          domain settings['domain']
+          user node['mac_os_x']['settings_user']
+          key k
+          value v
+          sudo true if settings['domain'] =~ /^\/Library\/Preferences/
+          global true if settings['domain'] =~ /^NSGlobalDomain$/
+  ##uh for multiples.. need a loop here
+       params[:killwhat].each do |killwhat|
+         notifies :run, "execute[killall #{killwhat.to_s}]" if ! params[:killwhat].to_a.empty?
+       end
+          notifies :run, "execute[killall Dock]" if settings['domain'] =~ /^com.apple.dock$/
+          notifies :run, "execute[killall Dock]" if settings['domain'] =~ /^com.apple.dashboard$/
+          notifies :run, "execute[killall Finder]" if settings['domain'] =~ /^com.apple.finder$/
+          notifies :run, "execute[killall loginwindow]" if settings['domain'] =~ /^com.apple.spaces$/
         end
-        return d
-end
-
-puts "#{mode} NewHash Array depth: #{hash_traverse(processwhat)}"
-
-puts "#{mode} New Array depth: #{count_subarrays(processwhat.to_a)}"
-puts "#{mode} wtf depth #{processwhat.class}"
-puts "#{mode} wtf else depth #{node['mac_os_x']['settings'].class}"
+      end
+    end
 
 
-#  log "updatedefaults: updating |depth #{depth}| [[ #{processwhat} ]]"
-
-
-
-#   ## ignore failure - depending on login state, these might not be running when chef runs.
-
-#   execute "killall Dock" do
-#     ignore_failure true
-#     action :nothing
-#   end
-#   execute "killall Finder" do
-#     ignore_failure true
-#     action :nothing
-#   end
-#   execute "killall loginwindow" do
-#     ignore_failure true
-#     action :nothing
-#   end
-
-
-#   params[:killwhat].each do |killwhat|
-#     log "DSR: killwhat #{killwhat}"
-#     execute "killall #{killwhat}" do
-#       ignore_failure true
-#       action :nothing
-#     end
-#   end
-
-
-
-#   processwhat.each do |domain,settings|
-#     settings.each do |k,v|
-#       next if k == 'domain'
-
-#       mac_os_x_userdefaults "#{settings['domain']}-#{k}" do
-#         domain settings['domain']
-#         user node['mac_os_x']['settings_user']
-#         key k
-#         value v
-#         sudo true if settings['domain'] =~ /^\/Library\/Preferences/
-#         global true if settings['domain'] =~ /^NSGlobalDomain$/
-# ##uh for multiples.. need a loop here
-#      params[:killwhat].each do |killwhat|
-#        notifies :run, "execute[killall #{killwhat.to_s}]" if ! params[:killwhat].to_a.empty?
-#      end
-#         notifies :run, "execute[killall Dock]" if settings['domain'] =~ /^com.apple.dock$/
-#         notifies :run, "execute[killall Dock]" if settings['domain'] =~ /^com.apple.dashboard$/
-#         notifies :run, "execute[killall Finder]" if settings['domain'] =~ /^com.apple.finder$/
-#         notifies :run, "execute[killall loginwindow]" if settings['domain'] =~ /^com.apple.spaces$/
-#       end
-#     end
-#   end
+  #using the default node attribute
+  if depth = 2
+    processwhat.each do |domain,settings|
+      settingsloop(settings)
+    end
+  ##passed as a param, so no outer loop
+  elsif depth = 1
+    settingsloop(processwhat)
+  else
+    raise "ERROR: userdefaults processwhat param mash is deeper than 2, so it must be broke.  should only be 1 or 2 deep."
+  end
 
 end
